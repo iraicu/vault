801265730ab8b3000486b5afb132b305697b7938
Ubuntu <ubuntu@torusnode17.maas>
2024-12-29 22:52:14 +0000

    final chia script

--- run_param_chia.sh
+++ run_param_chia.sh
@@ -14,11 +14,9 @@ case $(hostname) in
         # Configuration for epycbox machine
         if [ "$DISK_TYPE" == "HDD" ]; then
             temp_dir="/data-l/varvara/tmp"
-            secondary_temp_dir="/data-l/varvara/tmp2"
             plot_dir="/data-l/varvara/plot"
         elif [ "$DISK_TYPE" == "NVME" ]; then
             temp_dir="/data-fast/varvara/tmp"
-            secondary_temp_dir="/data-fast/varvara/tmp2"
             plot_dir="/data-fast/varvara/plot"
         else
             echo "Invalid disk type for epycbox. Use 'HDD' or 'NVME'."
@@ -26,19 +24,21 @@ case $(hostname) in
         fi
         output_dir="data/epycbox"
         threads_list=(1 2 4 8 16 32 64)
-        memory_list=(512 1024 2048 4096 8192 16384 32768 65536 131072 262144)
-        buckets_list=(64 128 256)
-        build="chia_x86"
+        buffer_range=(512 1024 2048 4096 8192 16384 32768 65536 131072 262144)
+        bucket_range=(16 32 64 128 256 512 1024)
+        stripe_min=32768
+        stripe_max=131072
+        max_threads=64
+        ram=262144
         ;;
+
     "orangepi5plus")
         # Configuration for orange pi 5
         if [ "$DISK_TYPE" == "HDD" ]; then
             temp_dir="/data-a/varvara/tmp"
-            secondary_temp_dir="/data-a/varvara/tmp2"
             plot_dir="/data-a/varvara/plot"
         elif [ "$DISK_TYPE" == "NVME" ]; then
             temp_dir="/data-fast/varvara/tmp"
-            secondary_temp_dir="/data-fast/varvara/tmp2"
             plot_dir="/data-fast/varvara/plot"
         else
             echo "Invalid disk type for orangepi5plus. Use 'HDD' or 'NVME'."
@@ -46,19 +46,21 @@ case $(hostname) in
         fi
         output_dir="data/opi5"
         threads_list=(1 2 4 8)
-        memory_list=(512 1024 2048 4096 8192 16384)
-        buckets_list=(64 128)
-        build="chia_arm"
+        buffer_range=(512 1024 2048 4096 8192 16384)
+        bucket_range=(16 32 64 128 256)
+        stripe_min=32768
+        stripe_max=65536
+        max_threads=32
+        ram=16384
         ;;
+
     "raspberrypi5")
         # Configuration for raspberry pi 5
         if [ "$DISK_TYPE" == "HDD" ]; then
             temp_dir="/data-a/varvara/tmp"
-            secondary_temp_dir="/data-a/varvara/tmp2"
             plot_dir="/data-a/varvara/plot"
         elif [ "$DISK_TYPE" == "NVME" ]; then
             temp_dir="/data-fast/varvara/tmp"
-            secondary_temp_dir="/data-fast/varvara/tmp2"
             plot_dir="/data-fast/varvara/plot"
         else
             echo "Invalid disk type for raspberrypi5. Use 'HDD' or 'NVME'."
@@ -66,21 +68,26 @@ case $(hostname) in
         fi
         output_dir="data/rpi5"
         threads_list=(1 2 4)
-        memory_list=(512 1024 2048 4096)
-        buckets_list=(64 128)
-        build="chia_arm"
+        buffer_range=(512 1024 2048 4096)
+        bucket_range=(16 32 64 128 256)
+        stripe_min=16384
+        stripe_max=32768
+        max_threads=16
+        ram=8192
         ;;
+
     *)
         # Default configuration for unknown machines
         echo "Unknown machine. Using default configuration."
         output_dir="data/default"
         threads_list=(4 8 16 32)
-        memory_list=(8192 16384 32768)
-        buckets_list=(64 128 256)
+        buffer_range=(8192 16384 32768)
+        bucket_range=(16 32 64 128 256)
+        stripe_min=32768
+        stripe_max=65536
         temp_dir="/mnt/default_temp"
-        secondary_temp_dir="/mnt/default_secondary"
-        build="chia_x86"
         ;;
+
 esac
 
 # Set the output file name
@@ -90,28 +97,31 @@ output_file="$output_dir/chia-param-C0-$DISK_TYPE.csv"
 k=27
 
 # Initialize the log file with headers
-echo "Threads,Memory,Buckets,Temp_Dir,Secondary_Temp_Dir,Plot_Dir,Elapsed_Time" > $output_file
+echo "Threads,Buffer,Buckets,Stripe,Temp_Dir,Plot_Dir,Elapsed_Time" > $output_file
 
 # Function to run a single test
 run_test() {
     local threads=$1
-    local memory=$2
+    local buffer=$2
     local buckets=$3
+    local stripe=$4
 
     start_time=$(date +%s)
-    chia plots create -k $k -r $threads -b $memory -u $buckets -t $temp_dir -2 $secondary_temp_dir -d $plot_dir -n 1
+    chia plotters chiapos --override-k -k $k -r $threads -b $buffer -u $buckets -s $stripe -t $temp_dir -d $plot_dir
     end_time=$(date +%s)
     
     elapsed_time=$((end_time - start_time))
-    echo "$threads,$memory,$buckets,$temp_dir,$secondary_temp_dir,$elapsed_time" >> $output_file
+    echo "$threads,$buffer,$buckets,$stripe,$temp_dir,$plot_dir,$elapsed_time" >> $output_file
 }
 
-# Iterate through parameter combinations and run the tests
+# Test combinations of all parameters
 for threads in "${threads_list[@]}"; do
-    for memory in "${memory_list[@]}"; do
-        for buckets in "${buckets_list[@]}"; do
-            echo "Testing: Threads=$threads, Memory=$memory, Buckets=$buckets, Temp=$temp_dir, Secondary_Temp=$secondary_temp_dir"
-            run_test $threads $memory $buckets
+    for buffer in "${buffer_range[@]}"; do
+        for buckets in "${bucket_range[@]}"; do
+            for stripe in $(seq $stripe_min $stripe_max $((stripe_max/2))); do
+                echo "Testing: Threads=$threads, Buffer=$buffer, Buckets=$buckets, Stripe=$stripe"
+                run_test $threads $buffer $buckets $stripe
+            done
         done
     done
 done
