d8452773d01ee48debef3765be9084594980b90f
Varvara Bondarenko <varvara@raspberrypi5.pi.matrix>
2025-01-04 18:35:11 +0000

    rpi5 run_chia

--- run_chia_param.sh
+++ run_chia_param.sh
@@ -68,7 +68,7 @@ case $(hostname) in
         threads_list=(1 2 4)
         buffer_range=(512 1024 2048 4096)
         bucket_range=(16 32 64 128)
-        stripe_range=(1024 2048 4096 8192 16384 32768 65536 131072 262144)
+        stripe_range=(1024 2048 4096 8192 32768 65536 131072 262144)
         max_threads=4
         ram=8192
         ;;
