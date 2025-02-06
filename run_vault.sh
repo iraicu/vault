d6e9e9a2a42fcee749b3d8d07f2cda33c9182ab6
Varvara B <vbondarenko@hawk.iit.edu>
2025-02-06 16:57:28 +0000

    run_vault.sh modified

--- run_vault.sh
+++ run_vault.sh
@@ -51,7 +51,7 @@ case $(hostname) in
         MAKE_TARGET="vault_x86"
 	if [ "$DISK_TYPE" == "HDD" ]; then
 	   IO_THREADS=64
-	   OUTPUT_FILE="log-C0-HDD.csv"
+	   OUTPUT_FILE="log-C0-HDD-optimal-parameters.csv"
 	elif [ "$DISK_TYPE" == "NVME" ]; then
     	   IO_THREADS=8
     	   OUTPUT_FILE="log-C0-NVMe.csv"
@@ -98,7 +98,7 @@ run_tests() {
 
     for k in $(seq $k_start $k_end)
     do
-        $CACHE_DROP_SCRIPT
+        $CACHE_DROP_SCRIPT $DISK_TYPE
         output=$(./vault -t $HASH_THREADS -o $SORT_THREADS -i $IO_THREADS -f vault$k.memo -m $RAM -k $k -w true)
         echo "$k,$RAM,$output" >> $OUTPUT_FILE
     done
